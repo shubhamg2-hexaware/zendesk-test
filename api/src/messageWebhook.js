89b707e2b444bdd8db731b0437f66fc1e099eccb
Shubham Gupta <shubhamg2@hexaware.com>
2018-03-30 17:18:35 +0530

    log added

--- api/src/messageWebhook.js
+++ api/src/messageWebhook.js
@@ -2,7 +2,7 @@ const processMessage = require('./processMessage');
 var db = require('../db/mysql.js');
 
 module.exports = (req, res) => {
-    //console.log(req.body);
+    console.log(req.body);
     if (req.body.object === 'page') {
         console.log(req.body.entry[0].messaging[0]);
         req.body.entry.forEach(entry => {
