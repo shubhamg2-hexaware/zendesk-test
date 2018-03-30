216b63d7d4d86a97e016683b8503827c41840e53
Shubham Gupta <shubhamg2@hexaware.com>
2018-03-30 17:11:05 +0530

    log added

--- api/src/messageWebhook.js
+++ api/src/messageWebhook.js
@@ -4,7 +4,7 @@ var db = require('../db/mysql.js');
 module.exports = (req, res) => {
     //console.log(req.body);
     if (req.body.object === 'page') {
-        console.log(req.body.entry);
+        console.log(req.body.entry[0].messaging[0]);
         req.body.entry.forEach(entry => {
             entry.messaging.forEach(event => {
                 if (event.message && event.message.text) {
